b116fd97d6b291838d54e5fdc588aaa12e670e17
rohitlodhii <rohitlodhi0225@gmail.com>
2025-07-26 12:40:50 +0530

    update the main

--- rent_predictor_backend/main.py
+++ rent_predictor_backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 from pydantic import BaseModel, Field
 import pickle
 import pandas as pd
@@ -47,3 +48,7 @@ def predict_rent(data: RentRequest):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app" , host="0.0.0.0" , port=8000)
